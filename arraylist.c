0fc43deacc708160ac17e97c46ad63635c3e05a6
Replit user <>
2023-03-31 19:32:48 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -65,13 +65,13 @@ void* get(ArrayList * l, int i){
   
   if(i >= l->size)return NULL; 
 
-  /*if(i<0){
+  if(i<0){
     for(int z=l->size; z>i; z--){
-    return l->data[i];} 
-  }*/
+    return l->data[z];} 
+  }
 
   //l->data = l->data[l->size];
-  return l->data[i];
+  //return l->data[i];
   
 }
 
