3de91b05d720e5c8702d1b5e33353077f383b9b5
Replit user <>
2023-03-31 19:44:11 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -64,14 +64,13 @@ void* pop(ArrayList * l, int i){
 void* get(ArrayList * l, int i){
   
   if(i >= l->size)return NULL;
-  //if(i>0)return l->data[i];
   
-  if(i<0){
+  if(i<0)
+  {
     for(int z=l->size; z>i; z--)
     {
-      return l->data[z];
+      return l->data[z-1];
     }
-  }
 
   //l->data = l->data[l->size];
   return l->data[i];
