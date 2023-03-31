bf6d3adc554847ed89e5338d93a66961de259881
Replit user <>
2023-03-31 15:49:17 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -43,10 +43,7 @@ void push(ArrayList * l, void * data, int i){
     l->data[z]=l->data[z-1]; 
   } 
     l->data[i] = data; 
-    l->size++; 
-
-    
-    
+    l->size++;
   
 
 }
@@ -60,9 +57,14 @@ void* pop(ArrayList * l, int i){
 }
 
 void* get(ArrayList * l, int i){
-  //if(i >= l->size)return NULL; 
+  if(i >= l->size)return NULL; 
 
-  //if(i<0)
+  if(i<0){
+     for(int z=l->size; z>i; z--){
+    l->data[z]=l->data[z-1]; 
+  } 
+    l->data[i] = l->data;
+  }
   return NULL;
 }
 
