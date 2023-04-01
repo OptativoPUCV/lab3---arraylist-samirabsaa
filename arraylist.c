62358d3eaebb2abc21f479fb75b0b6708191cee9
Replit user <>
2023-04-01 12:19:59 +0000

    new try

--- arraylist.c
+++ arraylist.c
@@ -51,14 +51,18 @@ void push(ArrayList * l, void * data, int i){
 void* pop(ArrayList * l, int i){
   l->data[i]= NULL; 
 
-  for(int z=l->size; z>i; z--)
+  /*for(int z=l->size; z>i; z--)
     {
       l->data[z]=l->data[z-1];
        
-    }
+    }*/
   
   //l->size--;
- return NULL;   
+
+  aux = l->data[l->size-1];
+  l->size--;
+  return aux;
+ //return NULL;   
 }
 
 void* get(ArrayList * l, int i){
@@ -66,15 +70,13 @@ void* get(ArrayList * l, int i){
   if(i >= l->size)return NULL;
   if(i<0)i = l->size+i;
     
-  /*if(i<0)
-  {
+  /*if(i<0){
     for(int z=l->size; z>i; z--)
       return l->data[z];
   }
   l->data = l->data[l->size];*/
-  //l->size--;
   return l->data[i];
-//return l->data[l->size-1];
+
 }
 
 int get_size(ArrayList * l){
